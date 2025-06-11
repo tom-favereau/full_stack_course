60786eaf916cb1e6b9d7361bf5fecac6b6e01b11
[afaureve] <[tom.favereautpe@gmail.com]>
2025-06-11 14:48:09 +0300

    exercice 1.3, exercice 1.4 and exercice 1.5

--- part1/introdemo/src/App.jsx
+++ part1/introdemo/src/App.jsx
@@ -1,24 +1,28 @@
 const App = () => {
-    const course = 'Half Stack application development'
-    const part1 = {
-        name: 'Fundamentals of React',
-        exercises: 10
-    }
-    const part2 = {
-        name: 'Using props to pass data',
-        exercises: 7
-    }
-    const part3 = {
-        name: 'State of a component',
-        exercises: 14
+    const course = {
+        name: 'Half Stack application development',
+        parts: [
+            {
+                name: 'Fundamentals of React',
+                exercises: 10
+            },
+            {
+                name: 'Using props to pass data',
+                exercises: 7
+            },
+            {
+                name: 'State of a component',
+                exercises: 14
+            }
+        ]
     }
 
     return (
         <div>
-            < Header course={course}/>
-            < Content parts={[part1, part2, part3]}/>
+            < Header course={course.name}/>
+            < Content parts={course.parts}/>
 
-            < Total total={part1.exercises+part2.exercises+part3.exercises} />
+            < Total parts={course.parts} />
         </div>
     )
 }
@@ -43,7 +47,7 @@ const Part = (props) => {
 }
 
 const Total = (props) => {
-    return (<p>Number of exercises {props.total}</p>)
+    return (<p>Number of exercises {props.parts[0].exercises+props.parts[1].exercises+props.parts[2].exercises}</p>)
 }
 
 
