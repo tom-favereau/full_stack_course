88ec23200934cc92ee4a42e4b65d4115a723f52a
[afaureve] <[tom.favereautpe@gmail.com]>
2025-06-12 14:01:31 +0300

    exercice 2.12 to 2.15

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
+import personService from "./services/persons.js";
 
 const App = () => {
     const [persons, setPersons] = useState([])
@@ -9,11 +10,11 @@ const App = () => {
     const [filter, setFilter] = useState('')
 
     useEffect(() => {
-        axios
-            .get('http://localhost:3001/persons')
-            .then(response => {
-                setPersons(response.data)
-                const maxId = Math.max(...response.data.map(p => p.id), 0)
+        personService
+            .getAll()
+            .then(data => {
+                setPersons(data)
+                const maxId = data.length > 0 ? Math.max(...data.map(elem => elem.id)) : 0
                 setId(maxId + 1)
             })
     }, [])
@@ -32,22 +33,56 @@ const App = () => {
 
     const addPerson = (event) => {
         event.preventDefault()
-        const newPerson = { id:id, name: newName, number: newNumber }
+
+        const existingPerson = persons.find(p => p.name === newName)
+        if (existingPerson) {
+            if (window.confirm(`${newName} is already added to phonebook, replace the old number with a new one?`)) {
+                const updatedPerson = { ...existingPerson, number: newNumber }
+
+                personService
+                    .update(existingPerson.id, updatedPerson)
+                    .then(returnedPerson => {
+                        setPersons(persons.map(p => p.id !== existingPerson.id ? p : returnedPerson))
+                        setNewName('')
+                        setNewNumber('')
+                    })
+                    .catch(error => {
+                        alert(`Information of ${newName} has already been removed from server`)
+                        setPersons(persons.filter(p => p.id !== existingPerson.id))
+                    })
+            }
+            return
+        }
+
+        const newPerson = {  name: newName, number: newNumber, id:id.toString() }
         const alreadyExists = persons.some(person => person.name === newName)
         if (alreadyExists) {
             alert(`${newName} is already added to phonebook`)
             return
         }
-        setPersons(persons.concat(newPerson))
-        setNewName('')
-        setNewNumber('')
-        setId(id+1)
+        personService
+            .create(newPerson)
+            .then(returnedPerson => {
+                setPersons(persons.concat(newPerson))
+                setNewName('')
+                setNewNumber('')
+                setId(id+1)
+            })
     }
 
     const personsToShow = persons.filter(person =>
         person.name.toLowerCase().includes(filter.toLowerCase())
         //person.name.toLowerCase().startsWith(filter.toLowerCase())
     )
+
+    const handleDelete = (id, name) => {
+        if (window.confirm(`Delete ${name} ?`)) {
+            personService
+                .remove(id.toString())
+                .then(() => setPersons(persons.filter(p => p.id !== id)))
+                .catch(error => console.log('fail', error))
+        }
+    }
 
     return (
         <div>
@@ -67,7 +102,7 @@ const App = () => {
             <h2>Numbers</h2>
             <div>
                 {personsToShow.map(elem =>
-                    <Person key={elem.id} person={elem}/>
+                    <Person key={elem.id} person={elem} handleDelete={handleDelete}/>
                 )}
             </div>
         </div>
@@ -98,10 +133,13 @@ const PersonFrom = ({newName, newNumber, handleNameChange, handleNumberChange, a
         </form>
     )
 }
-const Person = ({person}) => {
+const Person = ({person, handleDelete}) => {
 
     return (
-        <div>{person.name} {person.number}</div>
+        <div>
+            {person.name} {person.number}
+            <button onClick={() => handleDelete(person.id, person.name)}>delete</button>
+        </div>
     )
 }
 
