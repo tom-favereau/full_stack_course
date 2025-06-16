44debfa185058a17d2e963f52a42fcf6b81a289d
[afaureve] <[tom.favereautpe@gmail.com]>
2025-06-16 11:36:05 +0300

    exercice 4.1 and 4.2

---
+++ part4/bloglist/app.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+const middleware = require('./utils/middleware')
+const blogsRouter = require('./controllers/blogs')
+
+const app = express()
+
+mongoose.connect(config.MONGODB_URI)
+    .then(() => logger.info('Connected to MongoDB'))
+    .catch(error => logger.error('Error connecting to MongoDB:', error.message))
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
