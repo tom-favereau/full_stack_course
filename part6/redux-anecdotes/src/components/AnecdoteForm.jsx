76d08278a1536b28553a5229870bb9ce8ec90201
[afaureve] <[tom.favereautpe@gmail.com]>
2025-06-23 14:49:43 +0300

    exercice 6.1 to 6.8

---
+++ part6/redux-anecdotes/src/components/AnecdoteForm.jsx
@@ -0,0 +1,26 @@
+import {createAnecdote} from "../reducers/anecdoteReducer.js"
+import { useDispatch } from 'react-redux'
+
+
+const AnecdoteForm = () => {
+    //const anecdotes = useSelector(state => state)
+    const dispatch = useDispatch()
+    const addAnecdote = (event) => {
+        event.preventDefault()
+        const content = event.target.anecdeote.value
+        event.target.anecdeote.value = ''
+        dispatch(createAnecdote(content))
+    }
+
+    return (
+        <div>
+            <h2>create new</h2>
+            <form onSubmit={addAnecdote}>
+                <div><input name="anecdeote"/></div>
+                <button type="submit">create</button>
+            </form>
+        </div>
+    )
+}
+
+export default AnecdoteForm
